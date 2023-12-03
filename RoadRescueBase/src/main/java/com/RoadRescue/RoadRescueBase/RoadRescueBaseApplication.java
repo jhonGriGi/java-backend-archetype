610597049397705504dbc136ff624d8d5a970a56
SrFelipeS2 <andresvalencialopez244@gmail.com>
2023-12-02 21:12:44 -0500

    :tada: Arquetipo AppRoadRescue
    Este es el arquetipo o estructura base de la APP RoadRescue

---
+++ RoadRescueBase/src/main/java/com/RoadRescue/RoadRescueBase/RoadRescueBaseApplication.java
@@ -0,0 +1,13 @@
+package com.RoadRescue.RoadRescueBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadRescueBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadRescueBaseApplication.class, args);
+	}
+
+}
---
+++ RoadRescueBase/src/test/java/com/RoadRescue/RoadRescueBase/RoadRescueBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RoadRescue.RoadRescueBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadRescueBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
